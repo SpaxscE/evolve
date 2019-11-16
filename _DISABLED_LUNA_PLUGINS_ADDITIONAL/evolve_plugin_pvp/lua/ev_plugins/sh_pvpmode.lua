0e2ea5d79cb8e17c03adb56428bbcaa677bf2c44
Blu <swagnemite@gmx.de>
2019-11-16 03:16:43 +0100

    give default weapons in pvp mode

--- _DISABLED_LUNA_PLUGINS_ADDITIONAL/evolve_plugin_pvp/lua/ev_plugins/sh_pvpmode.lua
+++ _DISABLED_LUNA_PLUGINS_ADDITIONAL/evolve_plugin_pvp/lua/ev_plugins/sh_pvpmode.lua
@@ -53,7 +53,31 @@ end
 function PLUGIN:PlayerSpawn( ply )
 	if ply:GetNWBool( "EV_PVPMode", false ) then
 		ply:GodDisable()
-		ply:SetNWBool( "EV_GodMode", false ) 
+		ply:SetNWBool( "EV_GodMode", false )
+		timer.Simple(FrameTime(), function() 
+			if not IsValid( ply ) then return end
+			ply:StripWeapons()
+
+			ply:GiveAmmo( 256,	"Pistol", 		true )
+			ply:GiveAmmo( 256,	"SMG1", 		true )
+			ply:GiveAmmo( 5,	"grenade", 		true )
+			ply:GiveAmmo( 64,	"Buckshot", 	true )
+			ply:GiveAmmo( 32,	"357", 			true )
+			ply:GiveAmmo( 32,	"XBowBolt", 	true )
+			ply:GiveAmmo( 6,	"AR2AltFire", 	true )
+			ply:GiveAmmo( 100,	"AR2", 			true )
+
+			ply:Give( "weapon_crowbar" )
+			ply:Give( "weapon_pistol" )
+			ply:Give( "weapon_smg1" )
+			ply:Give( "weapon_frag" )
+			ply:Give( "weapon_physcannon" )
+			ply:Give( "weapon_crossbow" )
+			ply:Give( "weapon_shotgun" )
+			ply:Give( "weapon_357" )
+			ply:Give( "weapon_rpg" )
+			ply:Give( "weapon_ar2" )
+		end)
 	end
 end
 
